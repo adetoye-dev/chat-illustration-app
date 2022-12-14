53d4dbc751ba6bc72fab10be34920847de9d7015
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2022-12-14 12:01:28 +0100

    Create MessageOut component

---
+++ src/components/MessageOut.jsx
@@ -0,0 +1,9 @@
+const MessageOut = ({ text }) => {
+  return (
+    <div className="bg-red-500 my-2 rounded-xl p-2 rounded-br-md text-sm w-3/4 relative left-1/4">
+      {text}
+    </div>
+  );
+};
+
+export default MessageOut;
