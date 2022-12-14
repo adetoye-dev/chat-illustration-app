dd1f0176d572f73faf951ab5cc756d88fc641366
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2022-12-14 12:16:47 +0100

    Allow custom styling on messageIn component

--- src/components/MessageIn.jsx
+++ src/components/MessageIn.jsx
@@ -1,6 +1,8 @@
-const MessageIn = ({ text }) => {
+const MessageIn = ({ text, customStyle }) => {
   return (
-    <div className="bg-red-500 my-2 rounded-xl p-2 rounded-bl-md text-xs w-3/4">
+    <div
+      className={`bg-red-500 my-2 rounded-xl p-2 rounded-bl-md text-xs w-3/4 ${customStyle}`}
+    >
       {text}
     </div>
   );
