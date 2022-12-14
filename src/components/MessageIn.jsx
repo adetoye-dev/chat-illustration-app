c8048e9de0a887189cb072803ceb25c5c1a01f49
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2022-12-14 11:55:32 +0100

    Create message in component

---
+++ src/components/MessageIn.jsx
@@ -0,0 +1,9 @@
+const MessageIn = ({ text }) => {
+  return (
+    <div className="bg-red-500 my-2 rounded-xl p-2 rounded-bl-md text-xs w-3/4">
+      {text}
+    </div>
+  );
+};
+
+export default MessageIn;
