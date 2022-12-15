af04233ec881c1fd2fd15550369ee3c3176f2f23
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2022-12-15 11:32:08 +0100

    Make layout responsive and render page desc

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import SmartphoneUi from "./components/SmartphoneUi";
+import PageDesc from "./components/PageDesc";
 
 function App() {
   return (
-    <div className="App">
-      <SmartphoneUi />
+    <div className="App flex w-screen h-screen">
+      <div className="flex gap-14 lg:flex-row flex-col w-10/12 max-w-5xl m-auto items-center justify-center py-10 lg:p-0">
+        <SmartphoneUi />
+        <PageDesc />
+      </div>
     </div>
   );
 }
