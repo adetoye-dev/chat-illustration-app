6104908c0a693719aece0b8538d45fe756c6b3cc
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2022-12-15 07:41:28 +0100

    Configure tailwind to use custom colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,22 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "hsl(0, 0%, 100%)",
+      "pale-violet": "hsl(276, 100%, 81%)",
+      "moderate-violet": "hsl(276, 55%, 52%)",
+      "desaturated-dark-violet": "hsl(271, 15%, 43%)",
+      "grayish-blue": "hsl(206, 6%, 79%)",
+      "very-dark-desaturated-violet": "hsl(271, 36%, 24%)",
+      "dark-grayish-violet": "hsl(270, 7%, 64%)",
+      "light-magenta": "hsl(293, 100%, 63%)",
+      "light-violet": "hsl(264, 100%, 61%)",
+      "light-grayish-violet": "hsl(270, 20%, 96%)",
+      "very-dark-desaturated-violet": "hsl(271, 36%, 24%)",
+      "very-light-magenta": "hsl(289, 100%, 72%)",
+    },
   },
   plugins: [],
 };
