c56a7b1eadd3011309595e8ba798fe187a0751a8
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2022-12-15 12:30:57 +0100

    Render background component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import SmartphoneUi from "./components/SmartphoneUi";
 import PageDesc from "./components/PageDesc";
+import Background from "./components/Background";
 
 function App() {
   return (
     <div className="App flex w-screen h-screen">
-      <div className="flex gap-14 lg:flex-row flex-col w-10/12 max-w-5xl m-auto items-center justify-center py-10 lg:p-0">
+      <Background />
+      <div className="flex z-10 gap-14 lg:flex-row flex-col w-10/12 max-w-5xl m-auto items-center justify-center py-10 lg:p-0">
         <SmartphoneUi />
         <PageDesc />
       </div>
