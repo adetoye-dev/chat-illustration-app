8037953e0ccb876a4ada50712e38f238a45773f6
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2022-12-15 11:30:32 +0100

    Change styling for desktop layout

--- src/components/PageDesc.jsx
+++ src/components/PageDesc.jsx
@@ -1,8 +1,10 @@
 const PageDesc = () => {
   return (
-    <div>
-      <h1 className="text-3xl text-center font-bold">Simple booking</h1>
-      <p className="text-center">
+    <div className="max-w-md text-center lg:text-left">
+      <h1 className="text-4xl font-medium mb-5 text-very-dark-desaturated-violet">
+        Simple booking
+      </h1>
+      <p className="text-dark-grayish-violet">
         Stay in touch with our dog walkers through the chat interface. This
         makes it easy to discuss arrangements and make bookings. Once the walk
         has been completed you can rate your walker and book again all through
