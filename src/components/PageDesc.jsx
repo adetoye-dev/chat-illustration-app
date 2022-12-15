41405836a4b11d515aae7b4014c60451794f20e5
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2022-12-15 08:12:59 +0100

    Create Page desc component

---
+++ src/components/PageDesc.jsx
@@ -0,0 +1,15 @@
+const PageDesc = () => {
+  return (
+    <div>
+      <h1 className="text-3xl text-center font-bold">Simple booking</h1>
+      <p className="text-center">
+        Stay in touch with our dog walkers through the chat interface. This
+        makes it easy to discuss arrangements and make bookings. Once the walk
+        has been completed you can rate your walker and book again all through
+        the chat.
+      </p>
+    </div>
+  );
+};
+
+export default PageDesc;
