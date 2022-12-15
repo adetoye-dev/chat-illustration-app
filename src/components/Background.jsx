eba2265b36c68e7aeb6b218a8a3514b9c0e8e278
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2022-12-15 12:30:27 +0100

    create background components

---
+++ src/components/Background.jsx
@@ -0,0 +1,10 @@
+const Background = () => {
+  return (
+    <>
+      <div className="bg-left fixed left-0 top-0 bg-gradient-to-bl from-light-magenta to-light-violet lg:w-1/3 w-2/4 h-3/5 lg:h-4/5 rounded-br-full lg:rounded-b-full lg:-left-20"></div>
+      <div className="bg-right fixed right-0 bottom-0 bg-light-grayish-violet lg:w-1/3 w-2/4 h-3/5 lg:h-4/5 rounded-tl-full lg:rounded-t-full lg:-right-20"></div>
+    </>
+  );
+};
+
+export default Background;
